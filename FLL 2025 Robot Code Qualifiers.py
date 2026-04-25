c9180ca4b64bb0772187bb8f58f7e68e020cefc1
Anu12-ops <anoushkabiswas11@gmail.com>
2026-04-24 20:27:03 -0700

    Code for OIC
    Take a look at all of the code for team Control Alt Defeat for the OIC in Greece 2026.

--- FLL 2025 Robot Code Qualifiers.py
+++ FLL 2025 Robot Code Qualifiers.py
@@ -1,7 +1,7 @@
 # TO RUN CODE
 ''' 
 pybricksdev run ble --name "CoachBot" "c:\Git\Robot_Testing\alphaTesting.py"
-pybricksdev run ble --name "[ROBOT_NAME]" "[CODE_FILE_PATH]"
+pybricksdev run ble --name "[CRTL ALT DEFEAT (THE DEFEATER)]" "[CODE_FILE_PATH]"
 '''
 
 '''
@@ -39,188 +39,11 @@ driveBase = DriveBase(leftDriveMotor, rightDriveMotor, 56, 80)
 
 
 '''
-Mission Functions
+Missions (Ctrl Alt Defeat)
 '''
 
-'''
-Moves robot straight for 1000 mm
-'''
-def sample1():
-    driveBase.straight(1000, Stop.COAST)
-'''
-Turns robot 90 degrees right
-'''
-def sample2():
-     driveBase.straight(-1000, Stop.COAST)
-
-'''
-Moves robot in an arc with a radius of 100mm and for 90 degrees right
-'''
-def sample3():
-     driveBase.arc(500, 90)
-
 """
-SMYAN
-"""
-def mapReveal():
-    driveBase.settings(800, 850, 670, 500)
-    driveBase.straight(-20)
-    driveBase.use_gyro(True)
-    rightAttachmentMotor.run_angle(400,253, Stop.HOLD, False)
-    driveBase.straight(705, Stop.BRAKE) #moves to mission 2
-    leftAttachmentMotor.run_angle(300, 80, Stop.HOLD, False)
-    driveBase.turn(-53.125) #turn to align for mission 2
-    #wait(1000)
-    driveBase.straight(190, Stop.BRAKE) #moves into mission 2
-    rightAttachmentMotor.run_angle(250, -230, Stop.HOLD, False) #lifts land thing
-    driveBase.straight(-25, Stop.BRAKE) #comes back to lift land thing
-def surfaceBrushing():
-    #driveBase.settings(500,300,200,300)
-    driveBase.straight(-200) #moves back
-    leftAttachmentMotor.run_angle(200, -70)
-    #print(driveBase.settings())
-    driveBase.settings(100,100)
-    driveBase.turn(-44) #hits lever 1
-    driveBase.settings(800,2400,670,2100) 
-    wait(500)
-    driveBase.straight(75) #goes to pick up brush
-    # while not driveBase.stalled():
-    #     pass
-    # driveBase.straight(0)
-    leftAttachmentMotor.run_angle(300, 70) #picks up brush
-    driveBase.straight(-30)
-    driveBase.turn(-65) #turns towards home
-    driveBase.straight(480) #goes home
-
-"""
-AADI
-"""
-def mineshaftexplorerCarefulrecovery():
-    driveBase.use_gyro(True)   
-
-    driveBase.settings(900, 900, 900, 900)
-    driveBase.straight(-20)
-
-    driveBase.straight(630)
-    driveBase.turn(-1.5)
-
-    rightAttachmentMotor.run_angle(400, 345)
-    # return
-    driveBase.straight(-30)
-    rightAttachmentMotor.run_angle(450, -340)
-
-        # driveBase.use_gyro(False)
-        # driveBase.straight(410) #Align to wall
-        # driveBase.use_gyro(True)
-    driveBase.use_gyro(False)
-    driveBase.straight(410)
-    #wait(2500)
-    driveBase.stop()
-    driveBase.use_gyro(True)
-    driveBase.straight(-69)
-
-    driveBase.turn(91)
-    driveBase.straight(-22)
-    driveBase.settings(200, 200, 500, 500)
-    leftAttachmentMotor.run_angle(300, -230.348, wait=False)
-    rightAttachmentMotor.run_angle(300, 350)
-        #leftAttachmentMotor.run_angle(100, 15)
-
-    driveBase.straight(166)
-        
-    leftAttachmentMotor.run_angle(60, 49)
-    rightAttachmentMotor.run_angle(100, -185)
-    # leftAttachmentMotor.run_angle(80,-25)
-
-    driveBase.straight(-160)
-    driveBase.turn(108)
-    leftAttachmentMotor.run_angle(180, 70, wait=False)
-    driveBase.settings(900, 900, 900, 900)
-    driveBase.straight(800)
-
-"""
-ALINA
-"""
-def unravelsandSitemarkingOilrig():
-    driveBase.settings(straight_speed=900,straight_acceleration=900,turn_rate=500,turn_acceleration=500)
-    leftColorSensor.detectable_colors([Color.BLACK, Color.BLUE, Color.NONE, Color.GRAY])
-    #print(leftColorSensor.color(True))
-    driveBase.use_gyro(True)
-
-    driveBase.straight(-20)
-    driveBase.straight(415)
-    driveBase.turn(3.5)
-    driveBase.straight(70)
-    rightAttachmentMotor.run_angle(300, 327)
-    driveBase.straight(-40)
-    rightAttachmentMotor.run_angle(300, -327,Stop.HOLD, False)
-    driveBase.straight(-100)
-    driveBase.turn(-25)
-    driveBase.straight(250)
-    driveBase.turn(24)
-    driveBase.settings(300,100,500,500)
-    driveBase.straight(200)
-    driveBase.settings(straight_speed=900,straight_acceleration=2000,turn_rate=500,turn_acceleration=500)
-    driveBase.straight(-200)
-    driveBase.turn(-30)
-    driveBase.straight(250)
-    driveBase.turn(30)
-    driveBase.straight(300)
-    driveBase.turn(120)
-    driveBase.straight(130)
-    driveBase.turn(-25)
-    driveBase.straight(20)
-    leftAttachmentMotor.run_angle(300, -800)
-    driveBase.settings(900,2000,900,900)
-    driveBase.straight(-200)
-    driveBase.turn(-90)
-    driveBase.straight(700)
-
-"""
-SMITHI
-"""
-def whatsOnSaleTipTheScale():
-    driveBase.settings(500, 500)
-    driveBase.straight(-20)
-    driveBase.use_gyro(True)   
-    #driveBase.straight(300,Stop.HOLD, False)  
-    #leftAttachmentMotor.run_angle(200, -160, Stop.HOLD, False)
-    #driveBase.straight(546)
-    driveBase.straight(300)
-    driveBase.turn(10)
-    driveBase.straight(246)
-    #wait(8000)
-    #driveBase.straight(546)
-    leftAttachmentMotor.run_angle(200, -80, Stop.HOLD, False)
-    rightAttachmentMotor.run_angle(1000, 167)
-    driveBase.straight(10)
-    driveBase.settings(680,680)
-    #leftAttachmentMotor.run_angle(400, -80, Stop.HOLD, False)
-    leftAttachmentMotor.run_until_stalled(300) # Hooks into the scalepan
-    driveBase.straight(2)
-    leftAttachmentMotor.run_angle(400, -120) #pulls out the scalepan 
-    # driveBase.turn(3)
-    # driveBase.settings(300,300)
-    driveBase.straight(-300) #160
-
-    driveBase.straight(40)
-    #driveBase.turn(-20)
-    #driveBase.turn(20,Stop.HOLD,False)
-    rightAttachmentMotor.run_angle(600, -140, Stop.HOLD)
-    # leftAttachmentMotor.run_angle(400, -80, Stop.HOLD, False)
-    # leftAttachmentMotor.run_angle(400, 140) # Hooks into the scalepan
-    # driveBase.straight(2)
-    # leftAttachmentMotor.run_angle(400, -120) #pulls out the scalepan 
-    #driveBase.turn(-47)
-    driveBase.settings(950,950,950,950)
-    leftAttachmentMotor.run_angle(200,140, wait=False)
-    driveBase.curve(-400,80)
-    
-    
-   #driveBase.straight(-150)
-
-"""
-ARI
+ARIHANT
 """
 def siloForgeHeavyliftingWOS():
     driveBase.settings(900, 500, 900, 500)
@@ -229,162 +52,265 @@ def siloForgeHeavyliftingWOS():
     
     driveBase.settings(600, 600)
     driveBase.straight(-15)
-    driveBase.straight(383)
-    driveBase.turn(2)
+    driveBase.straight(363)
+    driveBase.turn(8)
     for i in range(3):
-        rightAttachmentMotor.run_angle(900, -320) 
-        rightAttachmentMotor.run_angle(300, 305)
-    driveBase.turn(-33)
-    driveBase.straight(340)
-    driveBase.turn(75.5)
+        rightAttachmentMotor.run_angle(700, -290) 
+        rightAttachmentMotor.run_angle(250, 285)
+    driveBase.straight(-29)
+    driveBase.turn(-43)
+    driveBase.straight(351)
+    driveBase.turn(81)
     rightColorSensor.detectable_colors([Color.BLACK, Color.WHITE, Color.NONE, Color.BROWN, Color.GREEN, Color.GRAY])
     driveBase.drive(200, 0)
     while not rightColorSensor.color(True) == Color.BLACK:
         pass
     driveBase.straight(0, Stop.HOLD)
-    driveBase.straight(30)
-    driveBase.turn(-3)
-    rightAttachmentMotor.run_angle(200, -230)
-    rightAttachmentMotor.run_angle(340, 220, wait=False)
-    # driveBase.straight(10) 
-    leftAttachmentMotor.run_angle(800, -900)
-    driveBase.straight(-27.8)
-    driveBase.turn(-40)
-    driveBase.arc(725,-80) 
-    # TO RUN CODE
+    driveBase.settings(400, 500, 700, 800)
+    driveBase.straight(33)
+    rightAttachmentMotor.run_angle(200, -275)
+    rightAttachmentMotor.run_angle(340, 305)
+    # driveBase.turn(5)
+    leftAttachmentMotor.run_angle(800, -950)
+    driveBase.straight(-150)
+    # driveBase.settings(900,900,900,900)
+    driveBase.turn(-52)
+    driveBase.straight(-125)
+    driveBase.turn(-30)
+    driveBase.straight(-700)
 
+"""
+SMITHI
+"""
+def whatsOnSaleTipTheScale():
+    driveBase.settings(550, 550)
+    driveBase.straight(-20)
+    driveBase.use_gyro(True)   
+    #driveBase.straight(300,Stop.HOLD, False)  
+    #leftAttachmentMotor.run_angle(200, -160, Stop.HOLD, False)
+    driveBase.straight(500)
+    #-driveBase.straight(300)
+    #-driveBase.turn(10)
+    #driveBase.straight(246)
+    #wait(8000)
+    #driveBase.straight(546)
+    #leftAttachmentMotor.run_angle(200, -80, Stop.HOLD, False)
+    rightAttachmentMotor.run_angle(1200, 167)
+    driveBase.straight(10)
+    driveBase.settings(680,680)
+    leftAttachmentMotor.run_angle(300, -80, Stop.HOLD, False)
+    leftAttachmentMotor.run_until_stalled(700,Stop.COAST,80) # Hooks into the scalepan
+    driveBase.straight(2)
+    leftAttachmentMotor.run_angle(600, -120) #pulls out the scalepan 
+    # driveBase.turn(3)
+    # driveBase.settings(300,300)
+    driveBase.straight(-190) #160, 300
+    wait(10)
+    rightAttachmentMotor.run_angle(600,-50)
+    driveBase.straight(40)
+    rightAttachmentMotor.run_angle(600, -140, Stop.HOLD)
+    # leftAttachmentMotor.run_angle(400, -80, Stop.HOLD, False)
+    # leftAttachmentMotor.run_angle(400, 140) # Hooks into the scalepan
+    # driveBase.straight(2)
+    # leftAttachmentMotor.run_angle(400, -120) #pulls out the scalepan 
+    #driveBase.turn(-47)
+    driveBase.settings(950,1250,950,1250)
+    leftAttachmentMotor.run_angle(200,140, wait=False)
+    # driveBase.straight(-200)
+    # driveBase.turn(-20,Stop.HOLD,False)
+    # driveBase.straight(-300)
+    # driveBase.straight(-400)
+    # driveBase.turn(60)
+    # driveBase.straight(-50)
+   #driveBase.straight(-150)
+    driveBase.straight(-600)
+    #driveBase.arc(-400,-50)
+    #leftAttachmentMotor.run_angle(500, -180)
+   
 
 """
 ANOUSHKA
 """
-# def forumStatuerebuild():
-#     driveBase.settings(900,900,900,900)
-#     driveBase.straight(-10)
-#     driveBase.use_gyro(True)
-#     driveBase.straight(200)
-#     #driveBase.turn(12)
-#     driveBase.turn(15)
-#     driveBase.straight(400)
-#     #driveBase.straight(600)
-#     driveBase.settings(200,200,100,200)
-#     driveBase.straight(300,Stop.NONE)
-#     rightColorSensor.detectable_colors([Color.BLACK,Color.NONE])
-#     while not rightColorSensor.color(True) == Color.BLACK:
-#         pass
-#     driveBase.straight(0)
-#     #driveBase.turn(27)
-#     rightAttachmentMotor.run_until_stalled(300)
-#     driveBase.turn(15)
-#     driveBase.drive(300,0)
-#     wait(1150)
-#     driveBase.straight(0)
-#     #driveBase.straight(-9,Stop.COAST)
-#     #driveBase.straight(-27)
-#     driveBase.turn(18,Stop.HOLD,False)
-#     #rightAttachmentMotor.run_angle(300,-170)
-#     #rightAttachmentMotor.run_until_stalled(-300)
-#     rightAttachmentMotor.run(-300)
-#     wait(400)
-#     rightAttachmentMotor.stop()
-#     #driveBase.straight(155)
-#     #driveBase.turn(-14)
-#     driveBase.settings(900,900,500,900)
-#     driveBase.straight(-20)
-#     driveBase.turn(-14)
-#     driveBase.straight(170)
-#     wait(20)
-#     driveBase.settings(900,900,300,900)
-#     driveBase.straight(-200)
-#     driveBase.turn(50)
-#     driveBase.straight(400)
-#     #driveBase.turn(45)
-#     #driveBase.straight(50)
-#     driveBase.arc(200,70)
-
-def forumStatuerebuild2():
+def forumStatuerebuild():
     driveBase.settings(900,900,900,900)
     driveBase.straight(-15)
-    driveBase.straight(200)
-    #driveBase.turn(12)
-    driveBase.turn(15)
-    driveBase.straight(400)
-    #driveBase.straight(600)
-    driveBase.settings(200,200,100,200)
+    rightAttachmentMotor.run_until_stalled(-275, duty_limit=70)
+    rightAttachmentMotor.hold()
+    driveBase.settings(900,600,900,900)
+    driveBase.straight(700)
+    driveBase.settings(200,100,200,200)
     driveBase.straight(300,Stop.NONE)
-    rightColorSensor.detectable_colors([Color.BLACK,Color.NONE])
-    while not rightColorSensor.color(True) == Color.BLACK:
+    rightColorSensor.detectable_colors([Color.BLACK,Color.WHITE,Color.NONE])
+    leftColorSensor.detectable_colors([Color.BLACK,Color.WHITE,Color.NONE])
+    while not rightColorSensor.color(True) == Color.BLACK and leftColorSensor.color(True) == Color.BLACK:
         pass
+    #####driveBase.turn(27) #18 too little
     driveBase.straight(0)
-    #driveBase.turn(27)
-    #rightAttachmentMotor.run_gle(300,240)
-    rightAttachmentMotor.run_until_stalled(300)
+    driveBase.turn(40)
+    # leftDriveMotor.run_angle(100,90,Stop.NONE)
+    # while not rightColorSensor.color(True) == Color.WHITE:
+    #     pass
+    # leftDriveMotor.stop()
+    rightAttachmentMotor.run_until_stalled(300, Stop.COAST,40)
     driveBase.settings(900,900,500,900) #100 was too slow
-    driveBase.turn(18) #15 not enough
-    driveBase.straight(150)
-    #driveBase.drive(300,0)
-    #wait(1150)
-    #driveBase.straight(0)
-    # #driveBase.straight(-18)
+    # driveBase.turn(10,wait=False)
+    driveBase.straight(75)
     driveBase.settings(900,900,300,900)
-    rightAttachmentMotor.run_angle(600,-60)
-    driveBase.turn(23)
-    driveBase.straight(100)
+    #rightAttachmentMotor.run_until_stalled(-275,Stop.COAST,120)
+    rightAttachmentMotor.run_angle(235,-120)
+    wait(250)
+    driveBase.turn(8)
+    rightAttachmentMotor.hold()
+    #driveBase.turn(12)
+    driveBase.turn(4)
+    driveBase.straight(135)
+    leftAttachmentMotor.run_angle(100,-220)
     driveBase.straight(-200)
-    driveBase.turn(37)
-    driveBase.straight(470)
-    driveBase.turn(50)
-    driveBase.straight(150)
-    #driveBase.turn(45)
-    #driveBase.straight(50)
-    #driveBase.arc(200,65)
-    
-    
+    driveBase.settings(900,900,900,900)
+    driveBase.turn(-57)
+    driveBase.straight(750)
+
+"""
+ALINA
+"""
+def unravelsandSitemarkingOilrig():
+    driveBase.settings(900,900,900,980)
+
+    driveBase.straight(-20)
+    driveBase.straight(200)
+    driveBase.turn(7)
+    driveBase.straight(285)
+    driveBase.turn(7)
+    driveBase.settings(100,100,900,980)
+    driveBase.straight(130)
+    driveBase.settings(900,900,900,980)
+    driveBase.turn(7)
+    driveBase.straight(-20)
+    rightAttachmentMotor.run_angle(300, 210, Stop.HOLD)
+    leftAttachmentMotor.run_angle(990, 3500, Stop.HOLD)
+    rightAttachmentMotor.run_angle(990, -150, Stop.HOLD)
+    driveBase.straight(-600)
+
+"""
+SMYAN
+"""
+def mapRevealSurfaceBrushing():
+    driveBase.settings(800, 850, 670, 500)
+    driveBase.use_gyro(True)
+    driveBase.straight(-15) #going back to square against the wall
+    driveBase.straight(742) #Approaching top soil
+    driveBase.turn(-43.5) #turning to drop the attachment
+    driveBase.straight(166) #dropping the attachment and pushing two top soils
+    rightAttachmentMotor.run_angle(500, -230) #picking up top soil
+    driveBase.settings(600,300,200,300) #Speed up
+    driveBase.straight(-250) #moving away from top soil
+    driveBase.settings(800, 850, 670, 500) #speed up
+    driveBase.turn(-29.5) #turning to surface brushing
+    driveBase.straight(100) #Hooking surface brushing
+    leftAttachmentMotor.run_angle(300, 80) #pick up the brush
+    driveBase.turn(-170) #Turn to deliver surface brushing & topsoil
+    driveBase.straight(100) #goes forward to deliver surface brush and topsoil
+    leftAttachmentMotor.run_angle(350, -100) #testing if this causes stall was 300 and stalling - changing to -100
+    driveBase.straight(-100)
+    driveBase.turn(-15)
+    driveBase.straight(120)
+    rightAttachmentMotor.run_until_stalled(350, Stop.COAST, 60) #was 200 amd stalling changed to 150 to verify going back to 200
+    driveBase.straight(-100)
+    leftAttachmentMotor.run_angle(350, 110) #Move arm backup
+    driveBase.turn(110)
+    driveBase.straight(525)
+"""
+AADI
+"""
+def mineshaftexplorerCarefulrecovery():
+    driveBase.settings(900, 900, 900, 900)
+    driveBase.straight(-20)
+
+    driveBase.straight(630)
+    driveBase.turn(-1.85)
+
+    rightAttachmentMotor.run_angle(400, 345)
+        # return
+    driveBase.straight(-30)
+    rightAttachmentMotor.run_angle(450, -340)
+
+            # driveBase.use_gyro(False)
+            # driveBase.straight(410) #Align to wall
+            # driveBase.use_gyro(True)
+    driveBase.use_gyro(True)
+    # driveBase.turn(4.6)
+    driveBase.use_gyro(False)
+    driveBase.straight(410)
+        #wait(2500)
+    driveBase.stop()
+    driveBase.use_gyro(True)
+    driveBase.straight(-78)
+    driveBase.settings(300,200,200,200)
+    driveBase.turn(90) #instead of 92
+    driveBase.straight(-28)
+    driveBase.settings(200, 200, 500, 500)
+    # driveBase.straight(23)
+    driveBase.use_gyro(True)
 
 
-    wait(1000)
-    #wait(250)
-    # rightAttachmentMotor.run(300)
-    # wait(400)
-    # rightAttachmentMotor.stop()
-    # # rightAttachmentMotor.run_angle(200,-240,Stop.HOLD,False)
-    # # driveBase.turn(33)
-    # # wait(1000000)
-    # #driveBase.straight(155)
-    # #driveBase.turn(-14)
-    # driveBase.settings(900,900,500,900)
-    # driveBase.straight(-20)
-    # driveBase.turn(-14)
-    # driveBase.straight(170)
-    # wait(20)
-    # driveBase.settings(900,900,300,900)
-    # driveBase.straight(-200)
-    # driveBase.turn(50)
-    # driveBase.straight(400)
-    # #driveBase.turn(45)
-    # #driveBase.straight(50)
-    # driveBase.arc(210,70)
+    #leftAttachmentMotor.run_angle(445, -240, wait=False)
+    leftAttachmentMotor.run_until_stalled(-445,Stop.COAST,50)
+    rightAttachmentMotor.run_angle(300, 330)
+    leftColorSensor.detectable_colors([Color.BLACK, Color.NONE, Color.BROWN, Color.WHITE, Color.GREEN])
+    rightColorSensor.detectable_colors([Color.BLACK, Color.NONE, Color.BROWN, Color.WHITE, Color.GREEN])
+    driveBase.settings(175)
+    driveBase.straight(10, Stop.NONE)
+    while not ((leftColorSensor.color(True)==Color.WHITE) and (rightColorSensor.color(True)==Color.WHITE)):
+        pass
+    # driveBase.straight(33)
+    # wait(3000)
+    # ##driveBase.turn(-5)
+    # wait(3000)
+    # driveBase.straight(95)
+    driveBase.straight(110)
+    # wait(2000)
+    #driveBase.straight(113)
+    #driveBase.turn(-3)
+    #50 for the lifting - sweet spot for taking it out
+    leftAttachmentMotor.run_angle(100,51, wait=False)
+    rightAttachmentMotor.run_angle(200,-280)
+    # driveBase.straight(100)
+    driveBase.settings(600, 600, 350, 350)
+    driveBase.straight(-180)
+    driveBase.turn(55)
+    driveBase.straight(110)
+    driveBase.turn(-43)
+    driveBase.straight(350)
+    driveBase.turn(-20)
+    driveBase.straight(450)
+    driveBase.leftAttachmentMotor.run_angle(100)
+    driveBase.turn(-50)
+    #driveBase.straight(300)
+    driveBase.straight(100)
+
+
+    
     
 '''
 Run Functions
 '''
 def run10():
-    mapReveal()
-    surfaceBrushing()
-
-def run20():
-    mineshaftexplorerCarefulrecovery()
-
-def run30():
-    unravelsandSitemarkingOilrig()
-   
-def run40():
-    whatsOnSaleTipTheScale()
-
-def run50():
     siloForgeHeavyliftingWOS()
 
+def run20():
+    whatsOnSaleTipTheScale()
+
+def run30():
+    forumStatuerebuild()
+   
+def run40():
+    unravelsandSitemarkingOilrig()
+
+def run50():
+    mapRevealSurfaceBrushing()
+
 def run60():
-    forumStatuerebuild2()
+    mineshaftexplorerCarefulrecovery()
 
 
 runs = [run10,run20,run30,run40,run50,run60]
@@ -468,4 +394,3 @@ Execute Code
 '''
 menuSystem = MenuSystem(runs, runNames, hub, driveBase, rightAttachmentMotor, leftAttachmentMotor, rightColorSensor, leftColorSensor)
 menuSystem.execute()
-
